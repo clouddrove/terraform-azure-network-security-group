3835ceb1b62e97da20eb41d4d3774902da759528
Anmol Nagpal <anmol.nagpal@porall.com>
2023-01-09 16:08:17 +0530

    inboud variable define

--- _example/example.tf
+++ _example/example.tf
@@ -7,8 +7,8 @@ module "resource_group" {
   source      = "clouddrove/resource-group/azure"
   version     = "1.0.1"
   label_order = ["name", "environment", ]
-  name        = "ota"
-  environment = "staging"
+  name        = "app-name"
+  environment = "test"
   location    = "Canada Central"
 }
 
@@ -18,8 +18,8 @@ module "vnet" {
   version     = "1.0.0"
   label_order = ["name", "environment"]
 
-  name                = "ota"
-  environment         = "staging"
+  name                = "app"
+  environment         = "test"
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   address_space       = "10.30.0.0/22"
@@ -44,7 +44,7 @@ module "subnet" {
 
   routes = [
     {
-      name                   = "rt-ota-staging"
+      name                   = "rt-app-test"
       address_prefix         = "0.0.0.0/0"
       next_hop_type          = "VirtualAppliance"
       next_hop_in_ip_address = "10.20.0.4"
@@ -58,8 +58,32 @@ module "network_security_group" {
   resource_group_location = module.resource_group.resource_group_location
   source                  = "../"
   label_order             = ["name", "environment"]
-  app_name                = "ota"
-  environment             = "staging"
+  app_name                = "app"
+  environment             = "test"
   subnet_ids              = module.subnet.default_subnet_id
   resource_group_name     = module.resource_group.resource_group_name
+  inbound_rules = [
+    {
+     name = "ssh" 
+     priority = 101
+     access = "Allow"
+     protocol = "Tcp"
+     source_address_prefix = "67.23.123.234/32"
+     source_port_range = "*"
+     destination_address_prefix = "0.0.0.0/0"
+     destination_port_range = "22"
+     description = "ssh allowed port"
+},
+    {
+     name = "https" 
+     priority = 102
+     access = "Allow"
+     protocol = "Tcp"
+     source_address_prefix = "*"
+     source_port_range = "*"
+     destination_address_prefix = "0.0.0.0/0"
+     destination_port_range = "22"
+     description = "ssh allowed port"
+}
+]
 }
