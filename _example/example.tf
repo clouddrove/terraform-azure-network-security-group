9ed3289b8284e0df8936c368945e1d40ba897a63
mahesh yadav <devopsmahesh.it@gmail.com>
2023-05-24 13:16:05 +0530

    fix: github action

--- _example/example.tf
+++ _example/example.tf
@@ -6,18 +6,16 @@ provider "azurerm" {
 module "resource_group" {
   source      = "clouddrove/resource-group/azure"
   version     = "1.0.2"
-  name        = "app-nsg"
+  name        = "app"
   environment = "test"
   label_order = ["name", "environment", ]
   location    = "Canada Central"
 }
 
 module "vnet" {
-  depends_on  = [module.resource_group]
-  source      = "clouddrove/vnet/azure"
-  version     = "1.0.2"
-  label_order = ["name", "environment"]
-
+  depends_on          = [module.resource_group]
+  source              = "clouddrove/vnet/azure"
+  version             = "1.0.2"
   name                = "app"
   environment         = "test"
   resource_group_name = module.resource_group.resource_group_name
